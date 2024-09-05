05527f70411eeacae5180a5c4a5ab038e4e3e467
anneloviseramstad <160476303+anneloviseramstad@users.noreply.github.com>
2024-09-05 12:04:18 +0200

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
-import { displayProducts } from "/handlers/products/displayProducts.js";
-import { displayProduct } from "/handlers/products/displayProduct.js";
-import { displayCart } from "/handlers/cart/displayCart.js";
+import { displayProducts } from "./handlers/products/displayProducts.js";
+import { displayProduct } from "./handlers/products/displayProduct.js";
+import { displayCart } from "./handlers/cart/displayCart.js";
 
 function router() {
   const { pathname } = window.location;
